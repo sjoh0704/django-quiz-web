8c5d33e81f48d33ced1c8b1bb347a87831dc9e1b
root <sjoh0704@daum.net>
2021-04-11 15:51:07 +0000

    add detail, result, vote views

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from  django.urls import path
-from .views import index
+from .views import index, detail, result, vote
 urlpatterns = [
     path('', index, name="index"),
-    
+    path('<int:question_id>/', detail, name='detail'),
+    path('<int:pk>/results', result, name='result'),
+    path('<int:pk>/vote', vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
+    question_list = Question.objects.order_by('-pub_date')
+    output = ""
+    for q in question_list:
+        output += q.question_text
+        output += ', '
+
+    return HttpResponse(output)
+
     return HttpResponse("hehehehe")
     
+
+def detail(request, question_id):  
+    return HttpResponse("문제보기: {}".format(question_id))
+
+
+def result(request, question_id):
+    return HttpResponse("문제 결과: {}".format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("내가 한 투표: {}".format(question_id))
+
